858e49e18b67b28011c98a011ce5979bbef8434d
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-06-24 23:20:09 +0200

    Added clean up to some tests

--- src/test/java/pl/restassured/tests/user/CreateUserTest.java
+++ src/test/java/pl/restassured/tests/user/CreateUserTest.java
@@ -1,5 +1,6 @@
 package pl.restassured.tests.user;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import pl.restassured.main.pojo.UserCreateResponse;
 import pl.restassured.main.pojo.user.User;
@@ -11,11 +12,12 @@ import static org.testng.Assert.assertEquals;
 
 public class CreateUserTest extends TestBase {
 
+    private User user;
     @Test
     public void givenUserWhenPostUserThenUserIsCreatedTest() {
 
         UserTestDataGenerator userTestDataGenerator = new UserTestDataGenerator();
-        User user = userTestDataGenerator.generateUser();
+        user = userTestDataGenerator.generateUser();
 
         UserCreateResponse response = given().body(user)
                 .when().post("user")
@@ -24,5 +26,16 @@ public class CreateUserTest extends TestBase {
         assertEquals(response.getCode(), 200);
         assertEquals(response.getType(), "unknown");
         assertEquals(response.getMessage(), user.getId().toString());
+    }
+
+    @AfterMethod
+    public void cleanUpAfterTest() {
+        UserCreateResponse response = given()
+                .when().delete("user/" + user.getUsername())
+                .then().statusCode(200).extract().as(UserCreateResponse.class);
+
+        assertEquals(response.getCode(), 200);
+        assertEquals(response.getType(), "unknown");
+        assertEquals(response.getMessage(), user.getUsername());
     }
 }
