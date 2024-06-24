9bc633a2b579cc9c34e5b31181f54b22630188a9
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-06-24 22:25:51 +0200

    Used auto generated data in tests

--- src/test/java/pl/restassured/tests/user/CreateUserTest.java
+++ src/test/java/pl/restassured/tests/user/CreateUserTest.java
@@ -1,28 +1,28 @@
 package pl.restassured.tests.user;
 
 import org.testng.annotations.Test;
+import pl.restassured.main.pojo.UserCreateResponse;
 import pl.restassured.main.pojo.user.User;
+import pl.restassured.main.test.data.UserTestDataGenerator;
 import pl.restassured.tests.testbase.TestBase;
 
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
 
 public class CreateUserTest extends TestBase {
 
     @Test
     public void givenUserWhenPostUserThenUserIsCreatedTest() {
-        User user = User.builder()
-                .id(445)
-                .username("john")
-                .firstName("John")
-                .lastName("Doe")
-                .email("johndoe")
-                .password("secret")
-                .phone("123456789")
-                .userStatus(123)
-                .build();
 
-        given().body(user)
+        UserTestDataGenerator userTestDataGenerator = new UserTestDataGenerator();
+        User user = userTestDataGenerator.generateUser();
+
+        UserCreateResponse response = given().body(user)
                 .when().post("user")
-                .then().statusCode(200);
+                .then().statusCode(200).extract().as(UserCreateResponse.class);
+
+        assertEquals(response.getCode(), 200);
+        assertEquals(response.getType(), "unknown");
+        assertEquals(response.getMessage(), user.getId().toString());
     }
 }
