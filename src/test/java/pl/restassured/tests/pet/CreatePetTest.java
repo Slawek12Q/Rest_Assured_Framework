ab17acb4fd1fae0c8c96140085bb7bfd2b3ecd0e
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-07-01 01:28:01 +0200

    Refactor cleanUp method

--- src/test/java/pl/restassured/tests/pet/CreatePetTest.java
+++ src/test/java/pl/restassured/tests/pet/CreatePetTest.java
@@ -1,15 +1,16 @@
 package pl.restassured.tests.pet;
 
+import org.apache.http.HttpStatus;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import pl.restassured.main.pojo.ApiResponse;
 import pl.restassured.main.pojo.pet.Pet;
 import pl.restassured.main.rop.CreatePetEndpoint;
+import pl.restassured.main.rop.DeletePetEndpoint;
 import pl.restassured.main.test.data.pet.PetTestDataGenerator;
 import pl.restassured.tests.testbase.TestBase;
 
-import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 
 
@@ -29,11 +30,10 @@ public class CreatePetTest extends TestBase {
 
     @AfterMethod
     public void cleanUpAfterTest() {
-        ApiResponse response = given()
-                .when().delete("/pet/" + actualPet.getId())
-                .then().statusCode(200).extract().as(ApiResponse.class);
 
-        assertEquals(response.getCode(), 200);
+        ApiResponse response = new DeletePetEndpoint().setPetId(actualPet.getId()).sendRequest().assertRequestSuccess().getResponseModel();
+
+        assertEquals(response.getCode(), HttpStatus.SC_OK);
         assertEquals(response.getType(), "unknown");
         assertEquals(response.getMessage(), actualPet.getId().toString());
     }
