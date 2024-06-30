c88ebdaae8b07bcee67e06f2105d3bb1e950daa2
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-07-01 01:34:39 +0200

    Code refactor

--- src/test/java/pl/restassured/tests/testbase/TestBase.java
+++ src/test/java/pl/restassured/tests/testbase/TestBase.java
@@ -2,12 +2,10 @@ package pl.restassured.tests.testbase;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import org.aeonbits.owner.ConfigFactory;
-import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeSuite;
 import pl.restassured.main.properties.EnvironmentConfig;
 
@@ -21,6 +19,6 @@ public class TestBase {
         RestAssured.basePath = environmentConfig.basePath();
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
         RestAssured.requestSpecification = new RequestSpecBuilder().setContentType(ContentType.JSON).build();
-        RestAssured.responseSpecification = new ResponseSpecBuilder().expectStatusCode(HttpStatus.SC_OK).build();
+//        RestAssured.responseSpecification = new ResponseSpecBuilder().expectStatusCode(HttpStatus.SC_OK).build();
     }
 }
