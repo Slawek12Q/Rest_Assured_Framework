9e7797e931ab15cbbec9f99331e6c0e191c6e451
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-07-01 20:00:49 +0200

    Add Before Test annotation

--- src/test/java/pl/restassured/tests/user/DeleteUserTests.java
+++ src/test/java/pl/restassured/tests/user/DeleteUserTests.java
@@ -2,19 +2,23 @@ package pl.restassured.tests.user;
 
 import com.github.javafaker.Faker;
 import org.apache.http.HttpStatus;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pl.restassured.main.rop.DeleteUserEndpoint;
 import pl.restassured.tests.testbase.TestBase;
 
 public class DeleteUserTests extends TestBase {
 
+    String username;
 
+    @BeforeTest
+    public void beforeTest() {
+        username = new Faker().name().username();
+        new DeleteUserEndpoint().setUsername(username).sendRequest();
+    }
 
     @Test
     public void givenNonExistingUserWhenDeletingUserThenUserNotFoundTest() {
-
-        String username = new Faker().name().username();
-
         new DeleteUserEndpoint().setUsername(username).sendRequest().assertStatusCode(HttpStatus.SC_NOT_FOUND);
     }
 }
